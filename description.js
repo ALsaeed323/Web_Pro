c5b892c409455434d49070f2be4dadd7d1a0a918
Ali <ali2110430@miuegypt.edu.eg>
2023-03-26 00:21:30 +0200

    Added js for plus minus counter

--- description.js
+++ description.js
@@ -26,3 +26,19 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+let minusBtn = document.getElementById("minus-btn");
+let count = document.getElementById("count");
+let plusBtn = document.getElementById("plus-btn");
+
+let countNum = 0;
+count.innerHTML = countNum;
+
+minusBtn.addEventListener("click", () => {
+	countNum -= 1;
+	count.innerHTML = countNum;
+});
+
+plusBtn.addEventListener("click", () => {
+	countNum += 1;
+	count.innerHTML = countNum;
+});
